642fb90b2f1021d5a204f2da8166eca22a7c8c39
Manenomyk <manenomichael99@gmail.com>
2024-01-20 12:03:55 +0300

    navbar styling

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,17 +10,18 @@ function Navbar() {
   return (
     <div className='text-white flex justify-between items-center h-24 mx-auto max-w-[1240px] '>
         <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Optimal Audit</h1>
-        <ul className='flex hidden'>
+        <ul className='hidden md:flex'>
             <li className='p-4'>Home</li>
             <li className='p-4'>Company</li>
             <li className='p-4'>About</li>
             <li className='p-4'>Servises</li>
             <li className='p-4'>Contact</li>
         </ul>
-        <div>
-            <AiOutlineMenu size={20} />
+        <div onClick={handleNav} className='block md:hidden'>
+            {!nav ? <AiOutlineClose size={20}/> :<AiOutlineMenu size={20} /> }
+            
         </div>
-        <div className='fixed left-0 top-0 w-[20%] h-full border-r border-r-gray-900 bg-[#000300]'>
+        <div className={!nav ? 'fixed left-0 top-0 w-[40%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-700' : 'fixed left-[-100%]'}>
         <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Optimal Audit</h1>
             <ul className='p-4'>
                 <li className='p-4 border-b border-gray-600'>Home</li>
