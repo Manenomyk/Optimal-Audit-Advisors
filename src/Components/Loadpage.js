df76db99bf7f417f61095edc25234f1e2a214de9
Manenomyk <manenomichael99@gmail.com>
2024-01-20 12:45:53 +0300

    load page designs

--- src/Components/Loadpage.js
+++ src/Components/Loadpage.js
@@ -4,9 +4,9 @@ function Loadpage() {
   return (
     <div className='text-white'>
         <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-            <p>Best and Quality accounting services</p>
-            <h1>At Optimal, We deliver</h1>
-            <p>Give us a bet and you won't regret</p>
+            <p className='text-[#00df9a] uppercase font-bold p-2'>Best and Quality accounting services</p>
+            <h1 className='md:text-6xl sm:text-5xl text-4xl md:py-6'>At Optimal, We deliver</h1>
+            <p className='md:text-2xl sm:text-xl text-l font-bold'>Give us a bet and you won't regret</p>
         </div>
     </div>
   )
