19708d499ea1b1e0015205a6fcc9bc27f51150a9
Manenomyk <manenomichael99@gmail.com>
2024-01-20 12:28:35 +0300

    load page instantiation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
 import Navbar from './Components/Navbar';
+import Loadpage from './Components/Loadpage';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Loadpage />
     </div>
   );
 }
---
+++ src/Components/Loadpage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Loadpage() {
+  return (
+    <div className='text-white'>
+        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+            <p>Best and Quality accounting services</p>
+            <h1>At Optimal, We deliver</h1>
+            <p>Give us a bet and you won't regret</p>
+        </div>
+    </div>
+  )
+}
+
+export default Loadpage
