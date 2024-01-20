1bfb6582e02016ce95138c757a2044a44fa95877
Manenomyk <manenomichael99@gmail.com>
2024-01-20 13:43:06 +0300

    page2 design

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css';
 import Navbar from './Components/Navbar';
 import Loadpage from './Components/Loadpage';
+import Details from './Components/Details';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Loadpage />
+      <Details />
     </div>
   );
 }
---
+++ src/Components/Details.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Details() {
+  return (
+    <div className='w-full bg-white py-16 px-4'>
+        <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+            qwertyui
+        </div>
+    </div>
+  )
+}
+
+export default Details
