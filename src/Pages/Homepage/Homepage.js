8cf4bdbb66c0dcf4d7247ef762dc2409e73a8d3d
Manenomyk <manenomichael99@gmail.com>
2024-01-24 09:12:09 +0300

    Navbar call

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+
+import Navbar from '../../Components/Navbar.js';
 
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <div>
+      <Navbar />
+      
+    </div>
   )
 }
 
