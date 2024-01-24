50ebaffe421e1043b1608a9de2c5e34a7bfcd693
Manenomyk <manenomichael99@gmail.com>
2024-01-24 09:19:01 +0300

    components call

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 import Navbar from '../../Components/Navbar.js';
+import Loadpage from '../../Components/Loadpage.js';
+import Newsletter from '../../Components/Newsletter.js';
+import Details from '../../Components/Details.js';
+import ServiceSection from '../../Components/ServiceSection.js';
+import Footer from '../../Components/Footer.js';
+
 
 function Homepage() {
   return (
     <div>
       <Navbar />
-      
+      <Loadpage />
+      <Newsletter />
+      <Details />
+      <ServiceSection />
+      <Footer />
     </div>
   )
 }
