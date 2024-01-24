e070cadfd8399c4e6f13f3cb54d6165dbf9104be
Manenomyk <manenomichael99@gmail.com>
2024-01-24 09:08:42 +0300

    Page routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import './App.css';
-import Navbar from './Components/Navbar';
-import Loadpage from './Components/Loadpage';
-import Details from './Components/Details';
-import Newsletter from './Components/Newsletter';
-import ServiceSection from './Components/ServiceSection';
-import Footer from './Components/Footer';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Homepage from './Pages/Homepage/Homepage';
+import About from './Pages/About/About';
+import Services from './Pages/Services/Services';
+import Company from './Pages/Company/Company';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Loadpage />
-      <Details />
-      <Newsletter />
-      <ServiceSection />
-      <Footer />
-    </div>
+    
+      <Router>
+        <Routes>
+          <Route path='/' element={<Homepage />} ></Route>
+          <Route exact path='/about' element={<About />} ></Route>
+          <Route exact path='/services' element={<Services />}></Route>
+          <Route exact path='/company' element={<Company />} ></Route>
+        </Routes>
+      </Router>
+
   );
 }
 
