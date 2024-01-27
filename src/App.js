55f66b9009b0d9ba66b5b0bc1d6c512b10e6bbdb
Manenomyk <manenomichael99@gmail.com>
2024-01-27 17:06:09 +0300

    company page deletion

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './Pages/Homepage/Homepage';
 import About from './Pages/About/About';
 import Services from './Pages/Services/Services';
-import Company from './Pages/Company/Company';
 
 function App() {
   return (
@@ -13,8 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Homepage />} ></Route>
           <Route exact path='/about' element={<About />} ></Route>
-          <Route exact path='/services' element={<Services />}></Route>
-          <Route exact path='/company' element={<Company />} ></Route>
+          <Route exact path='/services' element={<Services />}></Route> 
         </Routes>
       </Router>
 
