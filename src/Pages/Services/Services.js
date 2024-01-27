615f473fcfcbcab303f301d36393889ee5019303
Manenomyk <manenomichael99@gmail.com>
2024-01-27 13:41:24 +0300

    service sect3

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -3,6 +3,8 @@ import Navbar from '../../Components/Navbar';
 import Footer from '../../Components/Footer';
 import './Services.css';
 import consult from '../../Assets/consult.avif';
+import report from '../../Assets/report.svg';
+import Tax from '../../Assets/tax.png';
 
 function Services() {
   return (
@@ -35,34 +37,54 @@ function Services() {
         <img className='w-[500px] mx-auto my-3' src={consult} alt='accounting images' />
         </div>
       </div>
-
+<hr className='text-white' />
 
       <div className='w-full p-[2rem] px-4 bg-white'>
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-        <img className='w-[500px] mx-auto my-3' src={consult} alt='accounting images' />
+        <img className='w-[300px] mx-auto my-3' src={report} alt='accounting images' />
         <div className='flex flex-col justify-center'>
           <h1 className='text-[#00df9a] uppercase font-bold text-center mt-3'>Financial reporting and forecasting</h1>
-          <p className='md:text-2xl sm:text-xl text-lg font-bold py-2'>Search no further!</p>
+          <p className='md:text-2xl sm:text-xl text-lg font-bold py-2'>We've got you covered!</p>
                 <p>
-                We are your trusted partner in audit and accounting
-                 consultancy. We understand that in today's
-                  dynamic business environment, effective
-                   financial management is crucial for success. 
-                   Our dedicated team of experts is here to 
-                   guide you through the complexities of 
-                   auditing and accounting, helping you achieve
-                    financial transparency, compliance, and 
-                    strategic growth
+                we understand that in today's fast-paced business 
+                landscape, accurate financial reporting and insightful
+                 forecasting are the cornerstones of success. Our
+                  dedicated team of financial experts is here to 
+                  empower your business with unparalleled financial
+                   intelligence,
+                 driving strategic decision-making and sustainable growth.
                 </p>
                 <button className='bg-[#e4c131] w-[200px] rounded-md  my-6 mx-auto md:mx-0 p-2 text-black font-bold'>Read More</button>
         </div>
         
         </div>
-
-
-       
       </div>
 
+      <hr className='text-white' />
+
+
+      <div className='w-full p-[5rem] px-4 bg-white'>
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+      
+      <div className='flex flex-col justify-center'>
+        <h1 className='text-[#00df9a] uppercase font-bold text-center mt-3'>Tax compliance certificate application</h1>
+        <p className='md:text-2xl sm:text-xl text-lg font-bold py-2'>We Beat that hassle for you</p>
+              <p>
+              Unlock Business Confidence! Secure Your Success
+               with Our Hassle-Free Tax Compliance 
+               Certificate Application. We Simplify,
+                You Thrive - Let's Navigate the Path
+                 to Financial Compliance Together!"
+              </p>
+              <button className='bg-[#e4c131] w-[200px] rounded-md  my-6 mx-auto md:mx-0 p-2 text-black font-bold'>Read More</button>
+      </div>
+      <img className='w-[500px] mx-auto my-3' src={Tax} alt='accounting images' />
+      </div>
+
+      <hr className='text-white' />
+
+    </div>
+    
 
       <Footer />
     </div>
