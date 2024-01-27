f6774c9cf2e7861682ceeea90715e9ac8431d1fd
Manenomyk <manenomichael99@gmail.com>
2024-01-27 12:20:28 +0300

    servive background image

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import Navbar from '../../Components/Navbar'
+import Navbar from '../../Components/Navbar';
+import './Services.css';
 
 function Services() {
   return (
     <div>
       <Navbar />
+
+      <div className="background w-full h-[30rem] bg-cover bg-center" >
+      
+      </div>
     </div>
   )
 }
