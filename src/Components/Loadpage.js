470c0afb15e4fec6f3c91b7793c264c7604553b7
Manenomyk <manenomichael99@gmail.com>
2024-01-27 15:31:13 +0300

    Load page background

--- src/Components/Loadpage.js
+++ src/Components/Loadpage.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Load.css';
 
 function Loadpage() {
   return (
-    <div className='text-white'>
+    <div className='load text-white'>
         <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
             <p className='text-[#00df9a] uppercase font-bold p-2'>Best and Quality accounting services</p>
             <h1 className='md:text-6xl sm:text-5xl text-4xl md:py-6'>At Optimal, We deliver</h1>
