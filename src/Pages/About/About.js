f597b3f54f90ec5f679eb2872db3e91bf51a173f
Manenomyk <manenomichael99@gmail.com>
2024-01-27 14:49:38 +0300

    About page innitialization

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar'
+import Footer from '../../Components/Footer'
 
 function About() {
   return (
     <div>
     <Navbar />
+    <div className="background w-full h-[30rem] bg-cover bg-center" >
+    
+    </div>
+
+    <Footer />
     </div>
   )
 }
