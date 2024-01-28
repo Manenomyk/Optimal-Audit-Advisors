9aa3100d61a54b299bd70b9a309150b4e3c5d0c6
Manenomyk <manenomichael99@gmail.com>
2024-01-28 15:38:02 +0300

    react reveal servies page

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -9,6 +9,7 @@ import filling from '../../Assets/filling.svg';
 import reporting from '../../Assets/reporting.jpg';
 import { Helmet } from 'react-helmet';
 import * as save from "react-bootstrap";
+import * as rev from 'react-reveal';
 
 function Services() {
   return (
@@ -27,16 +28,21 @@ function Services() {
     <div className='w-full p-[2rem] px-4 bg-transparent'>
     <div className='max-w-[1240px] mx-auto grid md:grid-cols-3'>
       <div className='flex flex-col justify-self-center relative top-52'>
+      <rev.Zoom delay={200}>
         <p className='md:text-3xl sm:text-xl text-lg text-[#e4c131] font-bold py-2'> Expertise Meets Innovation:</p>
-              <p className='sm:text-xl text-white'>
-              
+        </rev.Zoom>
+        <p className='sm:text-xl text-white'>
+              <rev.Zoom delay={300}>
                Unleashing Tailored Solutions for Your Unique Needs
                
                Navigating Excellence, Delivering Results:
                 Because Your Business Deserves the Best!
+              </rev.Zoom>
               </p>
+              <rev.Zoom delay={400}>
               <a href='#services'><button className='bg-[#e4c131] w-[200px] rounded-md  my-6  p-2 text-black font-bold'>Explore More</button></a>
-      </div>
+              </rev.Zoom>
+            </div>
       
     </div>
   </div>
@@ -47,10 +53,14 @@ function Services() {
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
         
         <div className='flex flex-col justify-center'>
-        
-          <h1 className='text-[#00df9a] uppercase font-bold text-center mt-3'>Audit and accounting consultancy</h1>
-          <p className='md:text-2xl sm:text-xl text-lg font-bold py-2'>Search no further!</p>
-                <p>
+        <rev.Zoom delay={200}>
+            <h1 className='text-[#00df9a] uppercase font-bold text-center mt-3'>Audit and accounting consultancy</h1>
+          </rev.Zoom>
+          <rev.Zoom delay={300}>
+            <p className='md:text-2xl sm:text-xl text-lg font-bold py-2'>Search no further!</p>
+          </rev.Zoom>
+            <p>
+            <rev.Zoom delay={400}>
                 We are your trusted partner in audit and accounting
                  consultancy. We understand that in today's
                   dynamic business environment, effective
@@ -60,21 +70,33 @@ function Services() {
                    auditing and accounting, helping you achieve
                     financial transparency, compliance, and 
                     strategic growth
+              </rev.Zoom>
                 </p>
+                <rev.Zoom delay={400}>
                 <button className='bg-[#e4c131] w-[200px] rounded-md  my-6 mx-auto md:mx-0 p-2 text-black font-bold'>Read More</button>
-        </div>
+                </rev.Zoom>
+          </div>
+          <rev.Zoom delay={200}>
         <img className='w-[500px] mx-auto my-3' src={consult} alt='accounting images' />
+        </rev.Zoom>
         </div>
       </div>
 <hr className='text-white' />
 
       <div className='w-full p-[2rem] px-4 bg-white'>
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+        <rev.Zoom delay={200}>
         <img className='w-[300px] mx-auto my-3' src={report} alt='accounting images' />
+        </rev.Zoom>
         <div className='flex flex-col justify-center'>
+        <rev.Zoom delay={200}>
           <h1 className='text-[#00df9a] uppercase font-bold text-center mt-3'>Financial reporting and forecasting</h1>
+          </rev.Zoom>
+          <rev.Zoom delay={300}>
           <p className='md:text-2xl sm:text-xl text-lg font-bold py-2'>We've got you covered!</p>
-                <p>
+          </rev.Zoom>
+          <p>
+          <rev.Zoom delay={400}>
                 we understand that in today's fast-paced business 
                 landscape, accurate financial reporting and insightful
                  forecasting are the cornerstones of success. Our
@@ -82,9 +104,12 @@ function Services() {
                   empower your business with unparalleled financial
                    intelligence,
                  driving strategic decision-making and sustainable growth.
+             </rev.Zoom>
                 </p>
+                <rev.Zoom delay={400}> 
                 <button className='bg-[#e4c131] w-[200px] rounded-md  my-6 mx-auto md:mx-0 p-2 text-black font-bold'>Read More</button>
-        </div>
+                </rev.Zoom> 
+         </div>
         
         </div>
       </div>
@@ -96,18 +121,28 @@ function Services() {
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
       
       <div className='flex flex-col justify-center'>
+      <rev.Zoom delay={200}>
         <h1 className='text-[#00df9a] uppercase font-bold text-center mt-3'>Tax compliance certificate application</h1>
+        </rev.Zoom>
+        <rev.Zoom delay={300}>
         <p className='md:text-2xl sm:text-xl text-lg font-bold py-2'>We Beat that hassle for you</p>
-              <p>
+        </rev.Zoom>
+        <p>
+        <rev.Zoom delay={400}>
               Unlock Business Confidence! Secure Your Success
                with Our Hassle-Free Tax Compliance 
                Certificate Application. We Simplify,
                 You Thrive - Let's Navigate the Path
                  to Financial Compliance Together!"
+            </rev.Zoom>
               </p>
-              <button className='bg-[#e4c131] w-[200px] rounded-md  my-6 mx-auto md:mx-0 p-2 text-black font-bold'>Read More</button>
+              <rev.Zoom delay={400}>
+                <button className='bg-[#e4c131] w-[200px] rounded-md  my-6 mx-auto md:mx-0 p-2 text-black font-bold'>Read More</button>
+                </rev.Zoom>
       </div>
+      <rev.Zoom delay={200}>
       <img className='w-[500px] mx-auto my-3' src={Tax} alt='accounting images' />
+      </rev.Zoom>
       </div>
 
       <hr className='text-white' />
@@ -116,19 +151,29 @@ function Services() {
 
     <div className='w-full p-[2rem] px-4 bg-white'>
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-        <img className='w-[300px] mx-auto my-3' src={filling} alt='accounting images' />
-        <div className='flex flex-col justify-center'>
+      <rev.Zoom delay={200}>
+      <img className='w-[300px] mx-auto my-3' src={filling} alt='accounting images' />
+      </rev.Zoom>  
+      <div className='flex flex-col justify-center'>
+      <rev.Zoom delay={200}>
           <h1 className='text-[#00df9a] uppercase font-bold text-center mt-3'>NSSF, NHIF and PAYE filing</h1>
+          </rev.Zoom>
+          <rev.Zoom delay={300}>
           <p className='md:text-2xl sm:text-xl text-lg font-bold py-2'>You won't believe what we have for you</p>
-                <p>
+          </rev.Zoom>
+          <p>
+          <rev.Zoom delay={400}>
                 Experience Seamless Payroll Solutions for Your Success!
                  Elevate Your Business with Effortless NSSF,
                   NHIF, and PAYE Filing. Navigate the Future
                    of Payroll Management – Where Accuracy 
-                   Meets Efficiency
+                   Meets Efficiency.
+                   </rev.Zoom>
                 </p>
+                <rev.Zoom delay={400}>
                 <button className='bg-[#e4c131] w-[200px] rounded-md  my-6 mx-auto md:mx-0 p-2 text-black font-bold'>Read More</button>
-        </div>
+                </rev.Zoom>
+           </div>
         
       </div>
     </div>
@@ -139,21 +184,33 @@ function Services() {
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
       
       <div className='flex flex-col justify-center'>
+      <rev.Zoom delay={200}>
         <h1 className='text-[#00df9a] uppercase font-bold text-center mt-3'>Audit and Financial Reporting</h1>
+        </rev.Zoom>
+        <rev.Zoom delay={300}>
         <p className='md:text-2xl sm:text-xl text-lg font-bold py-2'>Precision Beyond Numbers, Insights Beyond Reports</p>
-              <p>
+        </rev.Zoom>
+        <p>
+        <rev.Zoom delay={400}>
               Beyond the Numbers: Unleash the Power of Precision
                with Our Audit Reporting Services! Your
                 Financial Guardians for Trustworthy Insights 
                 and Compliance Excellence. Elevate Your Business 
                 Story – Let's Audit Success Together!
                 <br />
+                </rev.Zoom>
+                <rev.Zoom delay={500}>
                 <b>#Audit Excellence</b> <br />
                 <b>#Financial Confidence!</b>
+                </rev.Zoom>
               </p>
+              <rev.Zoom delay={500}>
               <button className='bg-[#e4c131] w-[200px] rounded-md  my-6 mx-auto md:mx-0 p-2 text-black font-bold'>Read More</button>
-      </div>
+              </rev.Zoom>
+          </div>
+          <rev.Zoom delay={200}>
       <img className='w-[500px] mx-auto my-3' src={reporting} alt='accounting images' />
+      </rev.Zoom>
       </div>
 
     </div>
