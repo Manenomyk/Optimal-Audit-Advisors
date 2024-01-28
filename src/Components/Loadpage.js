4cd60cb0de734d06c0f74abf1ad4ab6ba54c29e9
Manenomyk <manenomichael99@gmail.com>
2024-01-28 14:16:22 +0300

    react reveal load page

--- src/Components/Loadpage.js
+++ src/Components/Loadpage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Load.css';
 import { Helmet } from 'react-helmet';
+import * as rev from 'react-reveal';
 
 function Loadpage() {
   
@@ -17,12 +18,22 @@ function Loadpage() {
       </Helmet>
     <div className='load w-full h-full bg-cover bg-center text-white'>
         <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        <rev.Zoom delay={400}>
             <p className='text-[#00df9a] uppercase font-bold p-2'>Best and Quality accounting services</p>
+          </rev.Zoom>
+          <rev.Zoom delay={500}>
             <h1 className='md:text-6xl sm:text-5xl text-4xl md:py-6'>At Optimal, We deliver</h1>
+          </rev.Zoom>
+          <rev.Zoom delay={600}>
             <p className='md:text-xl sm:text-l text font-bold'>Dare to Experience Excellence</p>
+            </rev.Zoom>
+            <rev.Zoom delay={600}>
             <p className='text-gray-300 p-4 font-bold'>We are you one stop place for all your audit and accounting needs</p>
+            </rev.Zoom>
+            <rev.Zoom delay={700}>
             <button className='bg-[#e4c131] w-[200px] rounded-md  my-6 mx-auto p-2 text-black font-bold'>Reach out</button>
-        </div>
+            </rev.Zoom>
+          </div>
     </div>
     </div>
     
